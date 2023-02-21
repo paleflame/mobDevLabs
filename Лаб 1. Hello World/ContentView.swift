da7a38cfb41c0b4b2808675a336aceb27d24d71f
Danil Vasilenko <85587508+paleflame@users.noreply.github.com>
2023-02-21 08:11:45 +0500

    Add files via upload

---
+++ Лаб 1. Hello World/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Lab1_HelloWorld
+//
+//  Created by Danil Vasilenko on 20.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State public var userInput: String = ""
+    @State public var output: String = "I repeat everything you write!"
+    
+    var body: some View {
+        VStack {
+            Text(output)
+                .font(.title)
+                .padding(.top, 100)
+            
+            Spacer()
+            
+            VStack
+            {
+                Divider()
+                TextField("Type something...", text: $userInput)
+                    .padding(10)
+                    .font(.system(size: 32))
+                Divider()
+            }
+            .padding(.bottom, 100)
+            
+            Button(action: {
+                output = userInput
+                userInput = ""
+            }) {
+                Text("Dispay input")
+                    .foregroundColor(.black)
+                    .padding(10)
+                    .border(Color.black, width: 2)
+                    .cornerRadius(4)
+            }
+            .font(.system(size: 32))
+        }
+      
+    }
+}
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
